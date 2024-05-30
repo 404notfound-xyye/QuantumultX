c604a91b7910f6e41ce648b07a8b8dfc047d6dcd
404NotFound_xyYe <xxye2022@gmail.com>
2024-05-30 17:32:33 +0800

    Update IP_API.js

--- Scripts/IP_API.js
+++ Scripts/IP_API.js
@@ -247,7 +247,7 @@ var flags = new Map([
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = flags.get(obj["countryCode"]) + " " + City_ValidCheck(obj["city"]) ;
-var subtitle = obj.as + " - " + ISP_ValidCheck(obj["org"] ;
+var subtitle = obj.as + " - " + ISP_ValidCheck(obj["org"]) ;
 var ip = obj["query"];
 var description =
   "AS:" +
